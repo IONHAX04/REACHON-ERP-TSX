e9769a39afe1b8629a00fa33c4676f19bb565a20
IONHAX04 <thirukumara04@gmail.com>
2025-06-04 13:05:52 +0530

    bulk booking drafted

--- src/renderer/src/components/15-BulkBooking/BulkBooking.tsx
+++ src/renderer/src/components/15-BulkBooking/BulkBooking.tsx
@@ -319,14 +319,6 @@ const BulkBooking: React.FC = () => {
     }
   }, [])
 
-  const formatDate = (date: Date) => {
-    const d = new Date(date)
-    const day = String(d.getDate()).padStart(2, '0')
-    const month = String(d.getMonth() + 1).padStart(2, '0') // months are 0-based
-    const year = d.getFullYear()
-    return `${day}-${month}-${year}`
-  }
-
   useEffect(() => {
     let filtered = [...customers]
 
